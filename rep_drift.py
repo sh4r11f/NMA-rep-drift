8f42c73cc668b8a6688be49dde5e7b5836ccfbb7
CiCi Zheng <xinzheng@CICIS-MAC-WORK.local>
2022-07-26 08:36:58 -0400

    doc strings for functions

--- rep_drift.py
+++ rep_drift.py
@@ -14,6 +14,18 @@ frames_per_repeat = int(n_frames/n_divs)
 
 def get_datasets(boc, container_id):
 
+    '''
+    Fetch the three experiments in one container, and order them according temporally. 
+
+    Args:
+        boc                  : allen SDK cache object
+        container_id(int)    : id of the experiment container
+
+    Returns:
+        datasets(list)       : list of 3 SDK experiment data object in the container
+        ordered_days(np arr) : length 3, treating date of exp1 as day 0.
+
+    '''
     exps_container = boc.get_ophys_experiments(experiment_container_ids= [container_id], stimuli=[stim_info.NATURAL_MOVIE_ONE])
     ids = [exp['id'] for exp in exps_container]
 
@@ -40,8 +52,17 @@ def get_datasets(boc, container_id):
 
 def get_response_vals(data_set):
     '''
-    input     : dataset
-    output    : cell_ids, trace vectors, running speed
+    Get the cell id information, and the two variables that we are interested in: 
+    1) response vector of n neurons for each stimuli;
+    2) mean running speed during that stimuli.
+
+    Args:
+        data_set             : SDK experiment data object
+
+    Returns:
+        cell_ids(np arr)     : (n_cells,), cell_ids of all neurons in this experiment
+        dff_vals(np arr)     : (n_repeats, n_divs, n_cells), response vectors for each stimuli, each repeat
+        run_vals(np arr)     : (n_repeats, n_divs), running speed for each stimuli, each repeat
     '''
 
     _, dff_traces = data_set.get_dff_traces()
@@ -49,12 +70,10 @@ def get_response_vals(data_set):
 
     stim_table = data_set.get_stimulus_table('natural_movie_one')
     running_speed = data_set.get_running_speed()[0]
-    # pupil_size = data_set.get_pupil_size()[1] 
 
     n_cells = dff_traces.shape[0]
 
     dff_vals = np.zeros((n_repeats, n_divs, n_cells))
-    # ppl_vals = np.zeros((n_repeats, n_divs))
     run_vals = np.zeros((n_repeats, n_divs))
 
     # for each repeat:
@@ -68,14 +87,17 @@ def get_response_vals(data_set):
                 div_idx*frames_per_repeat:(div_idx+1)*frames_per_repeat]     
             # average over block frames:
             dff_vals[repeat_idx, div_idx] = np.mean(dff_traces[:, div_repeat_idxs], axis=1)
-            # ppl_vals[repeat_idx, div_idx] = np.nanmean(pupil_size[div_repeat_idxs]) # there are NaNs
             run_vals[repeat_idx, div_idx] = np.mean(running_speed[div_repeat_idxs])
     
-    return cell_ids, dff_vals, run_vals # ppl_vals
+    return cell_ids, dff_vals, run_vals
 
 
 def shared_idx(in_session_cell, shared_cell):
     
+    '''
+    Helper func. Returns the index of shared_cell_ids for the cell_ids of a session. 
+    '''
+
     sorter = np.argsort(in_session_cell)
     in_session_idx = sorter[
         np.searchsorted(in_session_cell, shared_cell, sorter = sorter)]
@@ -85,6 +107,17 @@ def shared_idx(in_session_cell, shared_cell):
 
 
 def align_response_vals(datasets):
+    
+    '''
+    Identify neurons that are shared for all 3 datasets, and use only those neurons for cross session drift study. 
+
+    Args:
+        datasets(list)              : list of 3 SDK experiment data object in the container
+
+    Returns: 
+        dff_vals_container(list)    : list containing dff_vals for each of the 3 seessions, response vectors are only of the shared neurons.
+        run_vals_container(list)    : list containing run_vals for each of the 3 seessions
+    '''
 
     cell_ids_0, dff_vals_0, run_vals_0 = get_response_vals(datasets[0])
     cell_ids_1, dff_vals_1, run_vals_1 = get_response_vals(datasets[1])
@@ -107,12 +140,33 @@ def align_response_vals(datasets):
     return dff_vals_container, run_vals_container
 
 
+def get_align_angle(x, y):
+    '''
+    Helper Func. Returns angle between vecs in degrees.
+    '''
+    dot = np.dot(x,y)/(
+         np.linalg.norm(x) * np.linalg.norm(y))
+    if dot > 1.0:
+         dot = 1.0
+    elif dot < -1.0:
+        dot = -1.0
+    
+    return 180/np.pi * np.arccos(dot)
+
 
 def calc_similarity(dff_vals):
+    
     '''
-    input: response vectors 
-    output: correlation coef. matrix & angle matrix
+    Pearson correlation matrix and the angles between response vector matrix.
+
+    Args:
+        dff_vals(np arr)                : (n_repeats, n_divs, n_cells), response vectors for each stimuli, each repeat
+
+    Returns: 
+        within_session_corrs(np arr)    : (n_repeats*n_divs, n_repeats*n_divs), correlation coefficients of every two response vectors
+        within_session_angles(np arr)   : (n_repeats*n_divs, n_repeats*n_divs), angles in degrees of every two response vectors
     '''
+
     within_session_corrs = np.zeros((n_repeats*n_divs, n_repeats*n_divs))
     within_session_angles = np.zeros((n_repeats*n_divs, n_repeats*n_divs))
 
@@ -141,6 +195,16 @@ def calc_similarity(dff_vals):
 
 def calc_mean_vecs(dff_vals_container):
 
+    '''
+    Calculate mean response vector for a stimuli, over all repeats in a session.
+
+    Args:
+        dff_vals_container(list)  : list containing dff_vals for each of the 3 seessions, response vectors are only of the shared neurons
+
+    Returns: 
+        mean_vecs(np arr)         : (n_sessions, n_divs, n_shared_neurons), mean response vector
+    '''
+
     n_shared = dff_vals_container[0].shape[-1]
     mean_vecs = np.zeros((n_sessions, n_divs, n_shared))
 
@@ -153,12 +217,20 @@ def calc_mean_vecs(dff_vals_container):
 
 def effective_dimention(vars_explained):
 
+    '''
+    Helper func. Returns effective dimension of a variational space.
+    '''
+
     D = vars_explained.sum()**2/ (vars_explained**2).sum()
     
     return D
 
 
 def PCA_for_stimuli(dff_vals_stimuli, n_pcas):
+
+    '''
+    Helper func. Returns the sklearn PCA model object for response vectors to a stimuli of all repeats in one session.
+    '''
 
     pca_div = PCA(n_pcas)
     # fit on the 10 vecs:
@@ -174,6 +246,18 @@ def PCA_for_stimuli(dff_vals_stimuli, n_pcas):
 
 
 def do_PCA(dff_vals_container):
+
+    '''
+    For every stimulus group (response vectors to a stimuli of all repeats in one session), do PC decomposition.  
+
+    Args:
+        dff_vals_container(list)  : list containing dff_vals for each of the 3 seessions, response vectors are only of the shared neurons
+
+    Returns: 
+        pcas_session(list)        : 2-d list (3 x n_divs) containing pca models for each stimulus group
+        vars_session(np arr)      : (n_sessions, n_divs, n_repeats), variance explained of each pc direction (# of pcs = n_repeats)
+        dims_session(np arr)      : (n_sessions, n_divs), effective dimension of each stimulus groupx
+    '''
 
     n_shared = dff_vals_container[0].shape[-1]
     n_pcas = np.min((n_repeats, n_shared))
@@ -197,21 +281,31 @@ def do_PCA(dff_vals_container):
         
 
 
-# helper func:
-def get_align_angle(x, y):
-    'input: two vectors of same length; output: angle between vec in degrees'
-    dot = np.dot(x,y)/(
-         np.linalg.norm(x) * np.linalg.norm(y))
-    if dot > 1.0:
-         dot = 1.0
-    elif dot < -1.0:
-        dot = -1.0
-    
-    return 180/np.pi * np.arccos(dot)
+
 
 
 def drift_geometry(dff_vals_container, pcas_session, vars_session, mean_vecs):
 
+    '''
+    Function for correlating the geometric properties of the variational spaces before and after drift, for the three drift pairs:  1-2, 2-3, 1-3.
+
+    The geometric properties include: 
+    1) variance explained in each pc direction before and after drift;
+    2) drift magnitude in the early session space;
+    3) drift angle in the early session space.
+
+    Args:
+        dff_vals_container(list)        : list containing dff_vals for each of the 3 seessions, response vectors are only of the shared neurons
+        pcas_session(list)              : 2-d list (3 x n_divs) containing pca models for each stimulus group
+        vars_session(np arr)            : (n_sessions, n_divs, n_repeats), variance explained of each pc direction (# of pcs = n_repeats)
+        mean_vecs(np arr)               : (n_sessions, n_divs, n_shared_neurons), mean response vector
+    
+    Returns: 
+        pca1_var(np arr)                : (n_drifts, n_divs, n_repeats), variance explained matrix for the early sessions of the three drift_pairs.
+        pca_drift_magnitude(np arr)     : (n_drifts, n_divs, n_pcas), drift vector's magnitude in each pc direction.
+        pca_drift_align(np arr)         : (n_drifts, n_divs, n_pcas), drift vector's angle to each pc direction.
+        pca1_var_exp2_session(np arr)   : (n_drifts, n_divs, n_repeats), variance of the late sessions projected on to the early sessions for the three drift_pairs.
+    '''
 
     drift_pair_idxs = [[0, 1], [1, 2], [0, 2]]
     n_drifts = len(drift_pair_idxs)
@@ -256,14 +350,14 @@ def drift_geometry(dff_vals_container, pcas_session, vars_session, mean_vecs):
                 pca1_data1 = np.matmul(data1, pcas_session[session_idx1][div_idx].components_.T)
                 pca1_data2 = np.matmul(data2, pcas_session[session_idx1][div_idx].components_.T)
                 
-                pca1_data1_cov = np.cov(pca1_data1.T)
+                # pca1_data1_cov = np.cov(pca1_data1.T)
                 # pca1_data1_vars = np.diag(pca1_data1_cov)
                 # pca1_data1_vars_ratio = pca1_data1_vars/np.sum(pca1_data1_vars)
                 pca1_data2_cov = np.cov(pca1_data2.T)
                 pca1_data2_vars = np.diag(pca1_data2_cov)
                 pca1_var_exp2_session[drift_idx, div_idx] = pca1_data2_vars/np.sum(pca1_data2_vars)
 
-    pca1_var= np.zeros((n_sessions, n_divs, n_repeats))
+    pca1_var= np.zeros((n_drifts, n_divs, n_repeats))
 
     for session_idx in range(n_sessions):
         pca1_var[session_idx, :, :]  = vars_session[drift_pair_idxs[session_idx][0], :, :]
